61fd1000ef2e9ec1e3fc2acf4c3cde38db008ae8
ushiboy <ushiboy.dev@gmail.com>
2016-08-20 23:16:26 +0900

    add test environment

---
+++ test/ViewPager-test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+import { spy } from 'sinon';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+describe('ViewPager', () => {
+  it('should be true', () => {
+    assert.ok(false);
+  });
+});
