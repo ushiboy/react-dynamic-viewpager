93720e937c85c44b639bd57d2b66468b021783f5
ushiboy <ushiboy.dev@gmail.com>
2025-09-09 22:02:26 +0900

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      outDir: "lib",
+      insertTypesEntry: true,
+      exclude: ["**/*.test.*", "**/*.spec.*"],
+      tsconfigPath: "./tsconfig.app.json",
+      compilerOptions: {
+        declaration: true,
+        emitDeclarationOnly: true,
+        noEmit: false,
+      },
+    }),
+  ],
   build: {
+    outDir: "lib",
     lib: {
-      entry: "src/index.ts",
-      name: "react-dynamic-viewpager",
+      entry: {
+        index: resolve(__dirname, "src/index.ts"),
+        ViewPager: resolve(__dirname, "src/ViewPager.tsx"),
+        useViewPager: resolve(__dirname, "src/useViewPager.ts"),
+        type: resolve(__dirname, "src/type.ts"),
+      },
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          dir: "lib",
+          exports: "named",
         },
-      },
+        {
+          format: "cjs",
+          entryFileNames: "[name].cjs",
+          dir: "lib",
+          exports: "named",
+        },
+      ],
     },
   },
 });
